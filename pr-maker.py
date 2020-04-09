f7c649bfd6901b8c71aa45f8d5353196b139d0c2
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-04-09 13:24:57 -0700

    Add un added files

---
+++ pr-maker.py
@@ -0,0 +1,5 @@
+repo = "Server"
+upstream = "https://github.com/frc1678/server-2020/"
+text = input("PR name & number: ")
+new = text.split('#')
+print(f'{repo} PR#{new[1].rstrip()} `{new[0].rstrip().lstrip()}` {upstream}pull/{new[1]}')
